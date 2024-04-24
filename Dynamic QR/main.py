d55dbb4a9eeeb7a647075047f7a294f8ad6421fa
Killian O'Suilleabhain <killianosuilleabhain@gmail.com>
2024-04-24 17:02:55 +0100

    Succesfully Counts the Scans

--- Dynamic QR/main.py
+++ Dynamic QR/main.py
@@ -1,69 +1,108 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, redirect
 from flask_pymongo import PyMongo
 from werkzeug.utils import secure_filename
 import qrcode
+import hashlib
+import shortuuid
+from datetime import datetime
 
 app = Flask(__name__)
+# Configure the Flask app to use MongoDB
 app.config['MONGO_URI'] = 'mongodb+srv://kos:k0s@cluster0.g6zcayw.mongodb.net/database'
 mongo = PyMongo(app)
+
+url_collection = mongo.db.urls
+
+# Function to generate a shortened URL using a hash function
+def generate_short_url(url):
+    sha1_hash = hashlib.sha1(url.encode()).hexdigest()
+    return sha1_hash[:8]
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/shorten', methods=['POST'])
+def shorten():
+    long_url = request.form.get('long_url')
+    if long_url:
+        short_url = shortuuid.uuid()[:8]
+        full_short_url = request.host_url + short_url #testing
+        url_mapping = {
+            'short_url': short_url,
+            'long_url': long_url,
+            'clicks': 0  # Initialize click count
+        }
+        url_collection.insert_one(url_mapping)
+        return render_template('index.html', short_url=full_short_url)
+    else:
+        return redirect('/')
+
+@app.route('/<short_url>')
+def redirect_to_long_url(short_url):
+    url_data = url_collection.find_one({'short_url': short_url})
+    if url_data:
+        # Increment click count
+        url_collection.update_one({'short_url': short_url}, {'$inc': {'clicks': 1}})
+        return redirect(url_data['long_url'])
+    else:
+        return f'No URL found for {short_url}', 404
+
+
 @app.route('/generate_qr')
 def generate_qr():
-    data = request.args.get('data')
+    original_url = request.args.get('data')
     filename = request.args.get('filename') 
 
-    # Access the 'qr_codes' collection in MongoDB
-    qr_codes_collection = mongo.db.qr_codes
+    # Check if the URL already exists in the collection
+    url_entry = mongo.db.shortened_urls.find_one({'original_url': original_url})
 
-    # Check if the QR code already exists in the collection
-    qr_code = qr_codes_collection.find_one({'data': data, 'filename': filename})
-
-    if qr_code:
-        # If it exists, increment the usage counter
-        qr_codes_collection.update_one({'_id': qr_code['_id']}, {'$inc': {'uses': 1}})
+    if url_entry:
+        short_url = url_entry['short_url']
     else:
-        # If it doesn't exist, create a new record
-        new_qr_code = {
-            'data': data,
-            'filename': filename,
-            'uses': 1
-        }
-        qr_codes_collection.insert_one(new_qr_code)
+        short_url = generate_short_url(original_url)
+        mongo.db.shortened_urls.insert_one({
+            'original_url': original_url,
+            'short_url': short_url,
+            'clicks': 0  # Initialize click counter
+        })
 
-    # Generate QR code as before
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4,
     )
-    qr.add_data(data)
+    qr.add_data(short_url)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
 
-    # Secure the filename to prevent possible security issues
     secure_filename_str = secure_filename(filename)
-
-    # Save QR code with the specified filename
     temp_filename = f'{secure_filename_str}_qr.png'
     img.save(temp_filename)
 
-    # Return the QR code image
     return send_file(temp_filename, mimetype='image/png', as_attachment=True, download_name=f'{secure_filename_str}_qrcode.png')
 
 @app.route('/track_qr_code_usage')
 def track_qr_code_usage():
-    # Access the 'qr_codes' collection in MongoDB
-    qr_codes_collection = mongo.db.qr_codes
-
-    # Retrieve all documents from the collection
+    qr_codes_collection = mongo.db.shortened_urls
     qr_code_list = list(qr_codes_collection.find())
-
     return render_template('usage.html', qr_code_list=qr_code_list)
+
+# Route to handle short URLs and redirect to the original URL
+@app.route('/<short_url>')
+def redirect_to_original(short_url):
+    url_entry = mongo.db.shortened_urls.find_one({'short_url': short_url})
+    
+    if url_entry:
+        original_url = url_entry['original_url']
+        # Increment the click counter
+        mongo.db.shortened_urls.update_one({'short_url': short_url}, {'$inc': {'clicks': 1}})
+        # Redirect to the original URL
+        return redirect(original_url)
+    else:
+        # Handle the case when the short URL is not found
+        return render_template('not_found.html', short_url=short_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
